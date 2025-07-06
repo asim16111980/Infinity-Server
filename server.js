9fed22c3a69ab3825eedc62e746f0d663f82692c
asim16111980 <asim16111980@gmail.com>
2025-07-06 22:40:24 +0200

    Connect database

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-import { express } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
-const app = express;
+const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const starServer = async () => {
+  try {
+    const uri = process.env.ATLAS_URI;
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+
+    app.listen(port, () => {
+      console.log("Server is running on port:", port);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+starServer();
